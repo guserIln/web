5af9f71eaa568d7e26673e5110f02b8b5c76dfd2
guserIln <55316089+guserIln@users.noreply.github.com>
2020-08-08 14:09:23 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+defined('lab2') or die('Access denied');
+define('PATH','http://localhost/lab2/');
+define('MODEL', 'model/model.php');
+define('VIEWS', 'views/');
+define('CONTROLLER', 'controller/controller.php');
+define('TEMPLATE', VIEWS.'default/');
+define('PUB_IMAGE','pub_image/');
+
+define('HOST','localhost');
+define('DB','mysite');
+define('USER','root');
+define('PASS','');
+
+//mysqli_connect(HOST, USER, PASS)   or die('Connection failed...');
+
+//mysql_select_db(DB)   or die('Connection failed...');
+
+//mysql_query("SET NAMES UTF8");
