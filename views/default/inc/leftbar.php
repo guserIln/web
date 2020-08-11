42d96c3dbc970ce0228944eabda8d8d9038dcce4
guserIln <55316089+guserIln@users.noreply.github.com>
2020-08-11 18:30:29 +0300

    Add files via upload

---
+++ views/default/inc/leftbar.php
@@ -0,0 +1,12 @@
+<?php defined('lab2') or die('Access denied'); ?>
+<aside id="leftside">
+        <div class="title">
+            Информация </div>
+<p id="anim" onmouseover="moveTxt();"><strong >Телефон:</strong><br/>
+<span> 5896546883</span>
+</p>
+<p><strong>Режим работы:</strong><br />
+Будние дни: с 9:00 до 18:00 <br/>
+Суббота, Воскресенье - выходные
+</p>
+ </aside>
---
+++ views/default/inc/rightbar.php
@@ -0,0 +1,7 @@
+﻿<?php defined('lab2') or die('Access denied'); ?>
+<aside id="rightside">
+    <div class="title"> &nbsp; &nbsp; &nbsp; Карта и  информеры &nbsp; &nbsp; &nbsp; </div>
+ 
+<a  href="http://meteoinfo.ru/forecasts5000/russia/republic-bashkortostan/sterlitamak">
+<img align="center" src="http://www.meteoinfo.ru/informer/informer.php?ind=28825&type=3&color=56" alt="Гидрометцентр России" border="0" height="280" width="150"></a>
+ </aside>
