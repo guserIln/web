6f74d885cb0b67212e59aaa9bde9e6ebe88f9374
guserIln <55316089+guserIln@users.noreply.github.com>
2020-08-11 17:49:59 +0300

    Add files via upload

---
+++ views/default/detail.php
@@ -0,0 +1,176 @@
+<?php defined('lab2') or die('Access denied'); ?>
+
+<div class="kroshka">
+					<a href="#">Мобильные телефоны</a> / <a href="#">LG</a> / <span>Слайдеры</span>
+				</div>
+
+               <div class="catalog-detail">
+
+                <h1>Sony Xperia S 32Gb (черный)</h1>
+
+					<ul id="slideshow">
+						<li>
+							<h3></h3>
+							<span><?=PUB_IMAGE?>1.jpg</span>
+							<p></p>
+							<a href="#"><img src="thumbnails/1.jpg" alt="Small Fish" /></a>
+						</li>
+						<li>
+							<h3></h3>
+							<span><?=PUB_IMAGE?>2.jpg</span>
+							<p></p>
+							<a href="#"><img src="thumbnails/2.jpg" alt="Small Fish" /></a>
+						</li>
+						<li>
+							<h3></h3>
+							<span><?=PUB_IMAGE?>3.jpg</span>
+							<p></p>
+							<a href="#"><img src="thumbnails/3.jpg" alt="Small Fish" /></a>
+						</li>
+						<li>
+							<h3></h3>
+							<span><?=PUB_IMAGE?>4.jpg</span>
+							<p></p>
+							<a href="#"><img src="thumbnails/4.jpg" alt="Small Fish" /></a>
+						</li>
+						<li>
+							<h3></h3>
+							<span><?=PUB_IMAGE?>5.jpg</span>
+							<p></p>
+							<a href="#"><img src="thumbnails/5.jpg" alt="Small Fish" /></a>
+						</li>
+						<li>
+							<h3></h3>
+							<span><?=PUB_IMAGE?>6.jpg</span>
+							<p></p>
+							<a href="#"><img src="thumbnails/6.jpg" alt="Small Fish" /></a>
+						</li>
+						<li>
+							<h3></h3>
+							<span><?=PUB_IMAGE?>7.jpg</span>
+							<p></p>
+							<a href="#"><img src="thumbnails/7.jpg" alt="Small Fish" /></a>
+						</li>
+						<li>
+							<h3></h3>
+							<span><?=PUB_IMAGE?>8.jpg</span>
+							<p></p>
+							<a href="#"><img src="thumbnails/8.jpg" alt="Small Fish" /></a>
+						</li>
+					</ul>
+					<div id="wrapper">
+						<div id="fullsize">
+							<div id="imgprev" class="imgnav" title="Previous Image"></div>
+							<div id="imglink"></div>
+							<div id="imgnext" class="imgnav" title="Next Image"></div>
+							<div id="image"></div>
+							<div id="information">
+								<h3></h3>
+								<p></p>
+							</div>
+						</div>
+						<div id="thumbnails">
+						<div id="slideleft" title="Slide Left"></div>
+							<div id="slidearea">
+								<div id="slider"></div>
+							</div>
+							<div id="slideright" title="Slide Right"></div>
+						</div>
+					</div>
+
+               <div class="icon-detail">
+					<img src="<?=TEMPLATE?>images/ico-det-new.jpg" alt="новинка" />
+					<img src="<?=TEMPLATE?>images/ico-det-lider.jpg" alt="лидер продаж" />
+					<img src="<?=TEMPLATE?>images/ico-det-sale.jpg" alt="распродажа" />
+				</div>
+
+				<div class="short-opais">
+<h3>Краткое описание:</h3>
+<p>Футуристичный смартфон Sony Xperia S под управлением ОС Android 2.3 Gingerbread с возможностью обновления до версии 4.0 Ice Cream Sandwich – первый из серии Xperia NXT, линейки смартфонов нового поколения.</p>
+
+<p>В нем объединились новейшие технологии и лучшие разработки Sony, достойный функционал и минималистичный дизайн.</p>
+<p class="price-detail">Цена :  <span>24 990.</span></p>
+<a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-detail.jpg" alt="Добавить в карзину" /></a>
+				</div>
+
+				<div class="clr"></div>
+
+				<div class="long-opais">
+<h3>Описание телефона Sony Xperia S 32Gb (черный):</h3>
+<p>Футуристичный смартфон Sony Xperia S под управлением ОС Android 2.3 Gingerbread с возможностью обновления до версии 4.0 Ice Cream Sandwich – первый из серии Xperia NXT, линейки смартфонов нового поколения. В нем объединились новейшие технологии и лучшие разработки Sony, достойный функционал и минималистичный дизайн.</p>
+
+<p>Высокая производительность. Мощный двухъядерный процессор 1,5 ГГц Qualcomm Snapdragon MSM8260 гарантирует быстродействие любых игр и приложений.
+Превосходное качество изображения. 4,3-дюймовый HD 1280*720p экран Reality Display с технологией Mobile BRAVIA Engine обеспечивает потрясающие впечатления от просмотра изображений и видео с высоким разрешением.</p>
+
+<p>Удобство подключения к другим устройствам. Беспроводная связь на малых расстояниях (NFC) позволяет легко обмениваться информацией с другими пользователями и открывает доступ к многочисленным NFC-приложениям и услугам. Xperia S можно подключить к телевизору посредством Wi-Fi, DLNA, HDMI, а также использовать BRAVIA Sync для управления смартфоном с пульта ДУ телевизора.</p>
+
+				</div>
+
+				<div class="tech-harakt">
+					<h3>Технические характеристики</h3>
+
+<h4>Связь</h4>
+<ul>
+<li> - 3G  : Есть </li>
+<li> - EDGE  : Есть </li>
+<li> - Wi-Fi  : Есть (802.11 b/g/n)</li>
+</ul>
+
+<h4>Дисплей</h4>
+<ul>
+<li> - Тип дисплея  : TFT с LED подсветкой</li>
+<li> - Разрешение дисплея (пикс)  : 1280x720</li>
+<li> - Диагональ дисплея (дюйм)  : 4.3 </li>
+<li> - Сенсорный экран  : Есть </li>
+</ul>
+
+<h4>Система</h4>
+<ul>
+<li> - GPS-навигация  : Есть </li>
+<li> - Операционная система  : Android v2.3 </li>
+<li> - Частота проц. (МГц) : 1500 (Dual-core Qualcomm MSM8260 Snapdragon) </li>
+</ul>
+
+<h4>Мультимедиа</h4>
+<ul>
+<li> - Фотокамера : 12,1 Mпикс </li>
+<li> - Аудиоплеер : да </li>
+<li> - FM - радио : да </li>
+</ul>
+
+<h4>Корпус</h4>
+<ul>
+<li> - Тип корпуса : Классический </li>
+</ul>
+
+<h4>Память</h4>
+<ul>
+<li> - Встроенная память (Мб) : 32000</li>
+</ul>
+
+				</div>
+
+
+                <div class="clr"></div>
+
+
+            </div>
+<script type="text/javascript" src="<?=TEMPLATE?>js/script.js"></script>
+<script type="text/javascript">
+    $('slideshow').style.display='none';
+    $('wrapper').style.display='block';
+    var slideshow=new TINY.slideshow("slideshow");
+    window.onload=function(){
+        slideshow.auto=true;
+        slideshow.speed=5;
+        slideshow.link="linkhover";
+        slideshow.info="information";
+        slideshow.thumbs="slider";
+        slideshow.left="slideleft";
+        slideshow.right="slideright";
+        slideshow.scrollSpeed=4;
+        slideshow.spacing=5;
+        slideshow.active="#fff";
+        slideshow.init("slideshow","image","imgprev","imgnext","imglink");
+    }
+</script>
---
+++ views/default/hits.php
@@ -0,0 +1,53 @@
+<?php defined('lab2') or die('Access denied'); ?>
+<div class="catalog-index">
+    <h1><img src="<?=TEMPLATE?>images/lider-sale.jpg" alt="Лидеры продаж" /></h1>
+    <div class="product-index">
+        <h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+        <p>Цена:  <span>24 990.</span></p>
+        <a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" alt="Добавить в корзину" /></a>
+    </div>
+
+    <div class="product-index">
+        <h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+        <p>Цена:  <span>24 990.</span></p>
+        <a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" alt="Добавить в корзину" /></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+        <p>Цена:  <span>24 990.</span></p>
+        <a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" alt="Добавить в корзину" /></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+        <p>Цена:  <span>24 990.</span></p>
+        <a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" alt="Добавить в корзину" /></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+        <p>Цена:  <span>24 990.</span></p>
+        <a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" alt="Добавить в корзину" /></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+        <p>Цена:  <span>24 990.</span></p>
+        <a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" alt="Добавить в корзину" /></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+        <p>Цена:  <span>24 990.</span></p>
+        <a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" alt="Добавить в корзину" /></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="#">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href="#"><img src="<?=TEMPLATE?>images/phone-index.jpg" alt="" /></a>
+        <p>Цена:  <span>24 990.</span></p>
+        <a href="#"><img class="addtocard-index" src="<?=TEMPLATE?>images/addcard-index.jpg" alt="Добавить в корзину" /></a>
+    </div>
+</div>
